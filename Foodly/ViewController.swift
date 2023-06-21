9a3791981a9028c81dcc680dc46f986a50685a16
Orcun Erdil <orcun_erdil@hotmail.com>
2023-06-21 19:23:41 +0300

    Initial Commit

---
+++ Foodly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
