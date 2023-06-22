960db5388fe9ff248ea92711e3916b1be8d9579f
Orcun Erdil <orcun_erdil@hotmail.com>
2023-06-22 16:16:01 +0300

    The login screen has been designed and coded.

---
+++ Foodly/Extensions/UIView-Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView-Extension.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 21.06.2023.
+//
+
+import Foundation
---
+++ Foodly/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 22.06.2023.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
