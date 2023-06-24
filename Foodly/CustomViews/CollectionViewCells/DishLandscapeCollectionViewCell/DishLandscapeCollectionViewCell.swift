67e5f16b3f28d70c26b68ddabcd7e5cdad263ca6
Orcun Erdil <orcun_erdil@hotmail.com>
2023-06-24 19:08:41 +0300

    specials added.

---
+++ Foodly/CustomViews/CollectionViewCells/DishLandscapeCollectionViewCell/DishLandscapeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DishLandscapeCollectionViewCell.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 24.06.2023.
+//
+
+import UIKit
+
+class DishLandscapeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
