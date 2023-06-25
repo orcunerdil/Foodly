158adf735c9b3e602e56945039fadacaad320356
Orcun Erdil <orcun_erdil@hotmail.com>
2023-06-25 13:42:39 +0300

    food detail page added.

---
+++ Foodly/Extensions/UIViewController-Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController-Extension.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 25.06.2023.
+//
+
+import Foundation
---
+++ Foodly/Views/DishDetail/DishDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DishDetailViewController.swift
+//  Foodly
+//
+//  Created by Orçun Erdil on 24.06.2023.
+//
+
+import UIKit
+
+class DishDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
